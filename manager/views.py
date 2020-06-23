4957c1e053a846dcdfcb78b7376fd6d716508b23
nagi <ymxlinkinparkxlc@gmail.com>
2020-06-23 22:30:59 +0900

    Update templates Add Bootstrap

--- manager/views.py
+++ manager/views.py
@@ -1,17 +1,17 @@
 from django.views.generic import ListView
 
-from .models import *  # noqa
+from .models import *
 
 
 class WorkerListView(ListView):
-    model = Worker  # noqa
+    model = Worker
     template_name = 'worker_list.html'
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        workers = Worker.objects.all()  # noqa
+        workers = Worker.objects.all()
         context['workers'] = workers
 
         return context
